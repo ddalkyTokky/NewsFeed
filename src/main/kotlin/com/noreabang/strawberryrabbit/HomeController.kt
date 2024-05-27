dd83f5ad4eeb0220d4dc36b50ffad166507acb1d
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-27 17:18:47 +0900

    first setting

---
+++ src/main/kotlin/com/noreabang/strawberryrabbit/HomeController.kt
@@ -0,0 +1,12 @@
+package com.noreabang.strawberryrabbit
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+    @GetMapping
+    fun hello(): String {
+        return "hello"
+    }
+}

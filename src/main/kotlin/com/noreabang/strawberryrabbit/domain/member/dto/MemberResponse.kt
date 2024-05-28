166cd6853f91a4b13f64e9ef733f246a30d6aff1
eunhye han <hellou8363@gmail.com>
2024-05-28 16:21:30 +0900

    Feat: Member 관련 DTO 및 Entity 내 메서드 추가

---
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/member/dto/MemberResponse.kt
@@ -0,0 +1,8 @@
+package com.noreabang.strawberryrabbit.domain.member.dto
+
+data class MemberResponse (
+    val id: Long,
+    val email: String,
+    val nickname: String,
+    val image: String,
+)

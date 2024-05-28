1ebf0fa82f41c32bd3a8b5ba5858d06e71bda09b
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-28 11:08:08 +0900

    Feat: Music Entity 작성완료 DB validation 검증 완료

--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/music/model/Music.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/music/model/Music.kt
@@ -1,13 +1,19 @@
 package com.noreabang.strawberryrabbit.domain.music.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class Music {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
+
+    @Column(nullable = false, length = 100)
+    var singer: String? = null
+
+    @Column(nullable = false, length = 100)
+    var title: String? = null
+
+    @Column(length = 1000)
+    var cover: String? = null
 }

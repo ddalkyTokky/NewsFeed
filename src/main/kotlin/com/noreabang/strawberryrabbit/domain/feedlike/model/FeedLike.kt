a2bb725fdb8ead6bfc843cc1ba3a714d338058bb
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-28 11:36:39 +0900

    Feat: FeedLike Entity 작성완료 DB validation 검증 완료

--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/feedlike/model/FeedLike.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/feedlike/model/FeedLike.kt
@@ -1,5 +1,10 @@
 package com.noreabang.strawberryrabbit.domain.feedlike.model
 
-//@Entity
+import jakarta.persistence.EmbeddedId
+import jakarta.persistence.Entity
+
+@Entity(name = "feedlike")
 class FeedLike {
+    @EmbeddedId
+    val feedLikePK: FeedLikePK = FeedLikePK()
 }

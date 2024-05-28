7a1b678a9e9ae38aefb2a0b4a0e327165747a588
eunhye han <hellou8363@gmail.com>
2024-05-28 10:47:33 +0900

    Feat: Swageer-ui에서 로그인 테스트를 위한 Controller: signin, DTO: SigninRequest 추가

--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/member/controller/MemberController.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/member/controller/MemberController.kt
@@ -1,21 +1,20 @@
 package com.noreabang.strawberryrabbit.domain.member.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import com.noreabang.strawberryrabbit.domain.member.dto.SigninRequest
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/members")
 class MemberController {
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     // TODO : 회원가입 - 순용님 담당
 
-    // TODO : 로그인 구현 - 은혜 담당
-    @GetMapping
-    fun `swagger-ui test`() {
-        println("Hello world!")
-    }
 
+    // Swagger-ui에 보여주기 위함, 실제 login 처리는 Spring Security로 처리
+    @PostMapping("/signin")
+    fun singin (@RequestBody signinRequest: SigninRequest) {
+        log.info("signin ${signinRequest}")
+    }
 }

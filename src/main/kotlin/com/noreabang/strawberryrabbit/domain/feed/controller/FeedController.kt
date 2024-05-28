4993072bc52c8cfbf06f135823fe8d1c3f90b85e
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-28 10:40:07 +0900

    Feat: controller 추가

--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/controller/FeedController.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/controller/FeedController.kt
@@ -1,7 +1,34 @@
 package com.noreabang.strawberryrabbit.domain.feed.controller
 
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
+@RequestMapping("/feeds")
 @RestController
 class FeedController {
+
+    @GetMapping()
+    fun getFeedList(){
+
+    }
+
+    @GetMapping("/{feedid}")
+    fun getFeed() {
+
+    }
+
+    @PostMapping()
+    fun createFeed(){
+
+    }
+
+    @PutMapping("/{feedid}")
+    fun updateFeed(){
+
+    }
+
+    @DeleteMapping("/{feedid}")
+    fun deleteFeed(){
+
+    }
+
 }

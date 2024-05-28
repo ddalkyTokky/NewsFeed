cc9649e9c52914b49904a1812941c6487c6c6736
eunhye han <hellou8363@gmail.com>
2024-05-28 13:30:18 +0900

    Feat: CORS 설정을 위한 CorsConfigurationSource 추가

--- src/main/kotlin/com/noreabang/strawberryrabbit/infra/secutiry/CustomSecurityConfig.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/infra/secutiry/CustomSecurityConfig.kt
@@ -4,10 +4,28 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
 class CustomSecurityConfig {
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder() // 비밀번호 암호화
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+
+        configuration.setAllowedOriginPatterns(listOf("*")) // 모든 출처에서 요청 허용
+        configuration.allowedMethods = listOf("HEAD", "GET", "POST", "PUT", "DELETE") // 허용 HTTP 메서드 지정
+        configuration.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type") // 허용 HTTP 헤더 지정
+        configuration.allowCredentials = true // 자격 증명(쿠키, HTTP 인증, Client SSL 인증 등)
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration) // 모든 URL 패턴에 대해 설정한 configuration 적용
+
+        return source
+    }
 }

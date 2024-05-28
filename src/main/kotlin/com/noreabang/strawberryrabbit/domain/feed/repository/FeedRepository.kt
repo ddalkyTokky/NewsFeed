d4075c67c72a25e5bc025ce8ef0f8ed3d90cdbf8
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-28 16:54:55 +0900

    Feat: all 추가

--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/repository/FeedRepository.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/repository/FeedRepository.kt
@@ -1,6 +1,11 @@
 package com.noreabang.strawberryrabbit.domain.feed.repository
 
+import com.noreabang.strawberryrabbit.domain.feed.model.Feed
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
-//class FeedRepository : JpaRepository<Feed, Long>{
-//}
+interface FeedRepository : JpaRepository<Feed, Long>{
+    override fun findAll(pageable:Pageable): Page<Feed>
+    fun findAllByMemberNickname(nickname:String, pageable: Pageable): Page<Feed>
+}

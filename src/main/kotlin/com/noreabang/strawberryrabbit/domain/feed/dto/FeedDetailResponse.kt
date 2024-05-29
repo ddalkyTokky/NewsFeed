f65c1601fd8f20a2e14e6bf32f5e8e043f59908e
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-29 15:57:47 +0900

    Fix: MemberResponse로 변경

--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/dto/FeedDetailResponse.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/dto/FeedDetailResponse.kt
@@ -2,16 +2,16 @@ package com.noreabang.strawberryrabbit.domain.feed.dto
 
 import com.noreabang.strawberryrabbit.domain.comment.dto.CommentResponse
 import com.noreabang.strawberryrabbit.domain.feedlike.model.FeedLike
-import com.noreabang.strawberryrabbit.domain.member.model.Member
-import com.noreabang.strawberryrabbit.domain.music.model.Music
+import com.noreabang.strawberryrabbit.domain.member.dto.MemberResponse
+import com.noreabang.strawberryrabbit.domain.music.dto.MusicResponse
 import java.sql.Timestamp
 
 data class FeedDetailResponse(
 //    val id:Long,
     val title: String?,
     val content: String?,
-    val member: Member?,
-    val music: Music?,
+    val member: MemberResponse?,
+    val music: MusicResponse?,
     val createAt: Timestamp?,
 //    val feedLike: List<FeedLike>?,
 //    val comments: List<CommentResponse>
--- src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/dto/FeedResponse.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/domain/feed/dto/FeedResponse.kt
@@ -1,16 +1,15 @@
 package com.noreabang.strawberryrabbit.domain.feed.dto
 
 import com.noreabang.strawberryrabbit.domain.feedlike.model.FeedLike
-import com.noreabang.strawberryrabbit.domain.member.model.Member
-import com.noreabang.strawberryrabbit.domain.music.model.Music
+import com.noreabang.strawberryrabbit.domain.member.dto.MemberResponse
+import com.noreabang.strawberryrabbit.domain.music.dto.MusicResponse
 import java.sql.Timestamp
 
 data class FeedResponse (
     val title: String?,
     val content: String?,
     val createdAt: Timestamp?,
-    val member:Member?,
-//    val memberName: String?,
-    val music: Music?,
+    val member:MemberResponse?,
+    val music: MusicResponse?,
 //    val feedLike: List<FeedLike>?,
 )

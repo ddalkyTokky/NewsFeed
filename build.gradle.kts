f0181abba7a2bda3f5c13cee0f4ea69f4bd93fdb
eunhye han <hellou8363@gmail.com>
2024-05-29 21:27:11 +0900

    Feat: 메일 전송을 위한 JavaMailSender 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")

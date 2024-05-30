09c0f22ea4ad0c2c1f771a589f6d280aab041455
eunhye han <hellou8363@gmail.com>
2024-05-30 14:06:01 +0900

    Fix: 이메일 전송 요청 메서드의 반환타입 변경

--- src/main/kotlin/com/noreabang/strawberryrabbit/infra/email/EmailController.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/infra/email/EmailController.kt
@@ -2,6 +2,8 @@ package com.noreabang.strawberryrabbit.infra.email
 
 import com.noreabang.strawberryrabbit.infra.email.service.MailSendService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +18,8 @@ class EmailController(
 
     // 이메일 전송 후 인증 로직은 회원가입 서비스에서 진행
     @PostMapping("/check")
-    fun mailSend(@RequestBody @Valid emailRequest: EmailRequest): String {
-        return emailService.joinEmail(emailRequest.email)
+    fun mailSend(@RequestBody @Valid emailRequest: EmailRequest) : ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.OK).body(emailService.joinEmail(emailRequest.email))
     }
+    // TODO : 인증을 여러 번 연속 요청하는 경우에 대한 처리 필요
 }

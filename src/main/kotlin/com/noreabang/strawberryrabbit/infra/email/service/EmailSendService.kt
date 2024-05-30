488f0308ab24697bdabbaf28263a580df83ada83
eunhye han <hellou8363@gmail.com>
2024-05-30 14:10:17 +0900

    Feat: 인증 이메일을 보내기 전 DB에 이메일이 존재하는지 여부 검사 및 그에 따른 로직 추가

--- src/main/kotlin/com/noreabang/strawberryrabbit/infra/email/service/EmailSendService.kt
+++ src/main/kotlin/com/noreabang/strawberryrabbit/infra/email/service/EmailSendService.kt
@@ -1,7 +1,9 @@
 package com.noreabang.strawberryrabbit.infra.email.service
 
+import com.noreabang.strawberryrabbit.domain.member.service.MemberService
 import jakarta.mail.MessagingException
 import org.apache.logging.log4j.LogManager
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
@@ -11,7 +13,8 @@ import kotlin.random.Random
 @Service
 class MailSendService(
     private val mailSender: JavaMailSender,
-    private val redisService: RedisService
+    private val redisService: RedisService,
+    private val memberService: MemberService
 ) {
     private val logger = LogManager.getLogger()
     private var authNumber = ""
@@ -27,38 +30,48 @@ class MailSendService(
     }
 
     fun joinEmail(email: String): String {
-        makeRandomNumber()
-        val setFrom = "hellou2312@google.com" // EmailConfig에 설정한 이메일 주소
-        val toMail = email
-        val title = "딸기토끼의 노래방 회원 가입 인증 이메일 입니다." // 이메일 제목, content: 이메일 본문
-        val content = """ 
-               <div
-      style="
-        width: 700px;
-        height: 300px;
-        border: 1px solid black;
-        border-radius: 10px;
-        display: flex;
-        gap: 10px;
-        align-items: center;
-        justify-content: center;
-      "
-    >
-      <img
-        src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fx6E8T%2FbtsHHcdiXZ3%2Ftv1eQiyucK6FWGNPpVkFE0%2Fimg.jpg"
-        alt="딸기토끼"
-        style="width: 300px; height: 300px; border-radius: 10px; display: block"
-      />
-      <div>
-        <p>딸기토끼의 노래방 가입을 위한 인증 메일 입니다.</p>
-        <p>딸기토끼의 노래방을 이용해 주셔서 감사합니다.</p>
-        <p>아래 번호를 이용해 인증을 완료해 주세요.</p>
-        <p>인증 번호는 $authNumber 입니다.</p>
-      </div>
-    </div>
-        """.trimIndent()
-        mailSend(setFrom, toMail, title, content)
-        return authNumber
+        try {
+            val isExistEmail = memberService.getMemberByEmail(email)
+
+            if (isExistEmail != null) { // 이메일이 존재하는 경우
+                throw IllegalArgumentException("This email already exists.")
+            }
+
+            return email
+
+        } catch (e: EmptyResultDataAccessException) { // 데이터가 없는 경우 발생
+            logger.error(e.message)
+
+            val setFrom = "hellou2312@google.com" // EmailConfig에 설정한 이메일 주소
+            val toMail = email
+            val title = "딸기토끼의 노래방 회원 가입 인증 이메일 입니다." // 이메일 제목, content: 이메일 본문
+            val content = """
+                <div style="
+                    width: 700px;
+                    height: 300px;
+                    border: 1px solid black;
+                    border-radius: 10px;
+                    display: flex;
+                    gap: 10px;
+                    align-items: center;
+                    justify-content: center;">
+                  <img
+                    src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fx6E8T%2FbtsHHcdiXZ3%2Ftv1eQiyucK6FWGNPpVkFE0%2Fimg.jpg"
+                    alt="딸기토끼"
+                    style="width: 300px; height: 300px; border-radius: 10px; display: block"
+                  />
+                  <div>
+                    <p>딸기토끼의 노래방 가입을 위한 인증 메일 입니다.</p>
+                    <p>딸기토끼의 노래방을 이용해 주셔서 감사합니다.</p>
+                    <p>아래 번호를 이용해 인증을 완료해 주세요.</p>
+                    <p>인증 번호는 authNumber 입니다.</p>
+                  </div>
+                </div>
+            """.trimIndent()
+            mailSend(setFrom, toMail, title, content)
+
+            return "Sent an email"
+        }
     }
 
     fun mailSend(setFrom: String?, toMail: String?, title: String?, content: String?) { // 이메일 전송

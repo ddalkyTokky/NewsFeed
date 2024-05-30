6fef9f3e6c50a12ee3d85aa510df7c2ae08d7b6f
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-05-30 15:35:37 +0900

    Feat: file upload 기능 추가

---
+++ src/main/kotlin/com/noreabang/strawberryrabbit/infra/amazon/FileUploadService.kt
@@ -0,0 +1,31 @@
+package com.noreabang.strawberryrabbit.infra.amazon
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ObjectMetadata
+import io.jsonwebtoken.io.IOException
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+
+@Service
+class FileUploadService(
+    private val amazonS3Client: AmazonS3Client
+) {
+    @Value("\${CLOUD_AWS_S3_BUCKET}")
+    private lateinit var bucket: String
+
+    fun UploadFile(file: MultipartFile) : String? {
+        val image = listOf("jpg", "jpeg", "png", "gif", "bmp")
+        val fileName = file.originalFilename ?: throw IOException("File name is empty")
+        val exception = fileName.substringAfterLast('.')
+        if(!image.contains(exception)) return null
+//        val fileUrl = "https://$bucket/$fileName"
+        val metadata = ObjectMetadata().apply {
+            contentType = file.contentType
+            contentLength = file.size
+        }
+        //파일 저장하는부분
+        amazonS3Client.putObject(bucket, fileName, file.inputStream, metadata)
+        return amazonS3Client.getUrl(bucket, fileName).toString()
+    }
+}
